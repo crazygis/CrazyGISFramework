3d2c373088f707b46eca5593c4ed208c636625b2
crazygis <crazygis@crazygis.com>
2016-11-01 08:48:39 +0800

    update

---
+++ CrazyGIS.GeoPackage/GeoPackageManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyGIS.GeoPackage
+{
+	public class GeoPackageManager
+	{
+		/// <summary>
+		/// 构造函数
+		/// </summary>
+		public GeoPackageManager()
+		{
+
+		}
+
+		#region 接口方法
+
+		/// <summary>
+		/// 创建GeoPackage数据库
+		/// </summary>
+		/// <param name="gpkgFullName">数据库文件名称(含完整路径)</param>
+		/// <returns></returns>
+		public bool CreateGeoPackage(string gpkgFullName)
+		{
+			return true;
+		}
+
+		#endregion
+
+	}
+}
--- CrazyGISFramework/Program.cs
+++ CrazyGISFramework/Program.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CrazyGIS.GeoPackage;
-using CrazyGIS.Graphical;
 using CrazyGIS.CoordinateConversion;
-using CrazyGIS.CoordinateConversion.Models;
+using CrazyGIS.Graphical;
+using CrazyGIS.Graphical.Models;
+//using CrazyGIS.CoordinateConversion.Models;
 
 namespace CrazyGISFramework
 {
@@ -14,34 +14,44 @@ namespace CrazyGISFramework
 	{
 		static void Main(string[] args)
 		{
-			ConversionParameters parameters = new ConversionParameters();
-			// 七参数
-			SevenParameters sevenP = new SevenParameters();
-			sevenP.XaxisDeviation = 0;
-			sevenP.XaxisRotateRadian = 0;
-			sevenP.YaxisDeviation = 0;
-			sevenP.YaxisRotateRadian = 0;
-			sevenP.ZaxisDeviation = 0;
-			sevenP.ZaxisRotateRadian = 0;
-			sevenP.ScaleParameter = 0;
+			CoordinateTransform transform = new CoordinateTransform();
+			SpherePoint coor1 = new SpherePoint(112.97, 28.23);
+			SpherePoint result = transform.WGS84_To_GCJ02(coor1);
 
-			parameters.ConversionSevenParams = sevenP;
-			parameters.SourceCoordinateSystem = CoordinateSystemType.WGS84;
-			parameters.TargetCoordinateSystem = CoordinateSystemType.CGCS2000;
-			parameters.SourceCenterMeridian = 120;
-			parameters.TargetCenterMeridian = 120;
-			parameters.SourceCoordinateType = CoordinateType.Sphere;
-			parameters.TargetCoordinateType = CoordinateType.Sphere;
+			Console.WriteLine(result.lng + "," + result.lat);
+		}
 
-			ConversionService conversionService = new ConversionService(parameters);
+		static void SevenParamsConversion()
+		{
+			//ConversionParameters parameters = new ConversionParameters();
+			//// 七参数
+			//SevenParameters sevenP = new SevenParameters();
+			//sevenP.XaxisDeviation = 0;
+			//sevenP.XaxisRotateRadian = 0;
+			//sevenP.YaxisDeviation = 0;
+			//sevenP.YaxisRotateRadian = 0;
+			//sevenP.ZaxisDeviation = 0;
+			//sevenP.ZaxisRotateRadian = 0;
+			//sevenP.ScaleParameter = 0;
 
-			ICoordinate source = new SpherePoint(120, 32);
-			Console.WriteLine("源：" + source.xAxis + "," + source.yAxis);
-			ICoordinate target = conversionService.SourceToTarget(source);
-			Console.WriteLine("目标：" + target.xAxis + "," + target.yAxis);
+			//parameters.ConversionSevenParams = sevenP;
+			//parameters.SourceCoordinateSystem = CoordinateSystemType.WGS84;
+			//parameters.TargetCoordinateSystem = CoordinateSystemType.CGCS2000;
+			//parameters.SourceCenterMeridian = 120;
+			//parameters.TargetCenterMeridian = 120;
+			//parameters.SourceCoordinateType = CoordinateType.Sphere;
+			//parameters.TargetCoordinateType = CoordinateType.Sphere;
 
-			source = conversionService.TargetToSource(target);
-			Console.WriteLine("反算出来的源：" + source.xAxis + "," + source.yAxis);
+			//ConversionService conversionService = new ConversionService(parameters);
+
+			//CrazyGIS.CoordinateConversion.Models.ICoordinate source = 
+			//	new CrazyGIS.CoordinateConversion.Models.SpherePoint(120, 32);
+			//Console.WriteLine("源：" + source.xAxis + "," + source.yAxis);
+			//CrazyGIS.CoordinateConversion.Models.ICoordinate target = conversionService.SourceToTarget(source);
+			//Console.WriteLine("目标：" + target.xAxis + "," + target.yAxis);
+
+			//source = conversionService.TargetToSource(target);
+			//Console.WriteLine("反算出来的源：" + source.xAxis + "," + source.yAxis);
 		}
 	}
 }
